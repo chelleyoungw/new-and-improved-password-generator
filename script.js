6ffc580748ee7cc0ab6ca481188975cb785e13d9
Chelle Wood <chelleyoungw@gmail.com>
2024-01-17 01:03:18 -0600

    added first window prompt for password length

--- script.js
+++ script.js
@@ -9,9 +9,19 @@ function writePassword() {
   passwordText.value = password;
 
 }
-function generatePassword() {
-  return "hello world";
-}
 
+function generatePassword() {
+  var length = prompt("Choose your password length. Input a number between 8 and 128.");
+  var smallLength = (length >= 8);
+  var largeLength = (length <= 128);
+  
+
+  if (smallLength && largeLength) {
+    console.log('greater than 8');
+  }else {
+    console.log('wrong');
+  }
+  return "hello world";
+} 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
